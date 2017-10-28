9bb9a56c7663db37146b6c1b411abb23c7b76569
Steffen <steffen@infrabox.net>
2017-10-28 19:18:33 +0200

    don't clone in a container anymore

--- src/job/entrypoint.sh
+++ src/job/entrypoint.sh
@@ -15,4 +15,12 @@ else
     echo "Using host docker daemon socket"
 fi
 
+mkdir -p ~/.ssh
+
+if [ -f /tmp/gerrit/id_rsa ]; then
+    cp /tmp/gerrit/id_rsa ~/.ssh/id_rsa
+    chmod 600 ~/.ssh/id_rsa
+    ssh-keyscan -p $INFRABOX_GERRIT_PORT $INFRABOX_GERRIT_HOSTNAME >> ~/.ssh/known_hosts
+fi
+
 /job/job.py $@
