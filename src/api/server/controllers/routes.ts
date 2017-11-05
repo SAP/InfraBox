1d0c3f483b4486897974e122e3bc82bfed064fcb
Steffen <steffen@infrabox.net>
2017-11-05 11:08:32 +0100

    fixes

--- src/api/server/controllers/routes.ts
+++ src/api/server/controllers/routes.ts
@@ -1,7 +1,6 @@
 import { Router } from "express";
 
 const p = Router();
-p.use('/github/hook/', require('./hook.controller'));
 p.use('/project/', require('./project.controller'));
 p.use('/project/', require('./upload.controller'));
 p.use('/project/', require('./trigger.controller'));
