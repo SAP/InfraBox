8e625a17ce362d4e878c79b56c56efaf34b31595
Steffen <steffen@infrabox.net>
2017-11-18 16:50:18 +0100

    fix generator

--- infrabox/generator/generator.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-env
-cp /generator/tests.json /infrabox/output/tests.json
-cp /generator/deployments.json /infrabox/output/deployments.json
-cp /generator/infrabox.json /infrabox/output/infrabox.json
