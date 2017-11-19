a438cb44f121f1efbba3653a641a9215694336c6
Steffen <steffen@infrabox.net>
2017-11-19 11:47:43 +0100

    minor ui fixes

--- src/dashboard-client/config/dev.env.js
+++ src/dashboard-client/config/dev.env.js
@@ -4,6 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  DASHBOARD_HOST: '"localhost"',
-  API_PATH: '"http://localhost/api/dashboard/"',
+  DASHBOARD_HOST: '"localhost:3000"',
+  API_PATH: '"http://localhost:3000/api/dashboard/"',
 })
