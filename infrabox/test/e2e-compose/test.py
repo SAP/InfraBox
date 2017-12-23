4026224f79336aeb69c0a15fca6dfabffc5afb12
Steffen <steffen@infrabox.net>
2017-12-23 18:38:08 +0100

    add first docker-compose e2e tests

---
+++ infrabox/test/e2e-compose/test.py
@@ -0,0 +1,93 @@
+import unittest
+import os
+import subprocess
+import time
+import xmlrunner
+import requests
+
+from pyinfraboxutils.db import connect_db
+from pyinfraboxutils.token import encode_project_token
+
+class Test(unittest.TestCase):
+    job_id = '1514af82-3c4f-4bb5-b1da-a89a0ced5e6f'
+    user_id = '2514af82-3c4f-4bb5-b1da-a89a0ced5e6b'
+    build_id = '3514af82-3c4f-4bb5-b1da-a89a0ced5e6a'
+    project_id = '4514af82-3c4f-4bb5-b1da-a89a0ced5e6f'
+    token_id = '5514af82-3c4f-4bb5-b1da-a89a0ced5e6f'
+
+    def setUp(self):
+        conn = connect_db()
+        cur = conn.cursor()
+        cur.execute('''DELETE FROM job''')
+        cur.execute('''DELETE FROM auth_token''')
+        cur.execute('''DELETE FROM collaborator''')
+        cur.execute('''DELETE FROM project''')
+        cur.execute('''DELETE FROM "user"''')
+        cur.execute('''DELETE FROM source_upload''')
+        cur.execute('''DELETE FROM build''')
+        cur.execute('''DELETE FROM test_run''')
+        cur.execute('''DELETE FROM job_stat''')
+        cur.execute('''DELETE FROM measurement''')
+        cur.execute('''DELETE FROM test''')
+        cur.execute('''DELETE FROM job_markup''')
+        cur.execute('''INSERT INTO "user"(id, github_id, avatar_url, name,
+                            email, github_api_token, username)
+                        VALUES(%s, 1, 'avatar', 'name', 'email', 'token', 'login')''', (self.user_id,))
+        cur.execute('''INSERT INTO project(name, type, id)
+                        VALUES('test', 'upload', %s)''', (self.project_id,))
+        cur.execute('''INSERT INTO collaborator(project_id, user_id, owner)
+                        VALUES(%s, %s, true)''', (self.project_id, self.user_id,))
+        cur.execute('''INSERT INTO auth_token(project_id, id, description, scope_push, scope_pull)
+                        VALUES(%s, %s, 'asd', true, true)''', (self.project_id, self.token_id,))
+        conn.commit()
+
+        os.environ['INFRABOX_CLI_TOKEN'] = encode_project_token(self.token_id, self.project_id)
+        os.environ['INFRABOX_API_URL'] = 'http://nginx-ingress/api'
+
+    def run_it(self, cwd):
+        command = ['infrabox', 'push', '--show-console']
+        process = subprocess.Popen(command, shell=False,
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.STDOUT,
+                                   cwd=cwd, universal_newlines=True)
+
+        # Poll process for new output until finished
+        msg = ""
+        while True:
+            line = process.stdout.readline()
+            if not line:
+                break
+
+            line = line.rstrip()
+            msg += line
+            print line
+
+        process.wait()
+
+        exitCode = process.returncode
+        if exitCode != 0:
+            raise Exception(msg)
+
+    def test_docker_job(self):
+        self.run_it('/infrabox/context/infrabox/test/e2e/tests/docker_job')
+
+    def test_docker_compose_job(self):
+        self.run_it('/infrabox/context/infrabox/test/e2e/tests/docker_compose_job')
+
+
+if __name__ == '__main__':
+    while True:
+        time.sleep(1)
+        try:
+            r = requests.get('http://nginx-ingress')
+
+            if r.status_code == 200:
+                break
+        except:
+            pass
+        print "Server not yet ready"
+
+    connect_db() # Wait for DB
+
+    with open('results.xml', 'wb') as output:
+        unittest.main(testRunner=xmlrunner.XMLTestRunner(output=output))
---
+++ infrabox/test/gen-compose/patch.py
@@ -0,0 +1,47 @@
+import copy
+import os
+import yaml
+
+data = None
+with open('/infrabox/output/e2e/compose/docker-compose.yml') as i:
+    data = yaml.load(i)
+
+del data['services']['api']['image']
+data['services']['api']['build'] = {
+    'context': '../../../../..',
+    'dockerfile': 'src/api-new/Dockerfile'
+}
+
+del data['services']['nginx-ingress']['image']
+data['services']['nginx-ingress']['build'] = {
+    'context': '../../../../..',
+    'dockerfile': 'src/docker-compose/ingress/Dockerfile'
+}
+
+env = None
+if os.environ.get('INFRABOX_CLI', None):
+    env = ['INFRABOX_DOCKER_COMPOSE_PROJECT_PREFIX=infrabox']
+else:
+    env = ['INFRABOX_DOCKER_COMPOSE_PROJECT_PREFIX=repo']
+
+data['services']['scheduler']['environment'] += env
+
+data['services']['test'] = {
+    'build': {
+        'context': '../../../../..',
+        'dockerfile': 'infrabox/test/e2e-compose/Dockerfile'
+    },
+    'environment': [
+        'INFRABOX_DATABASE_HOST=postgres',
+        'INFRABOX_DATABASE_DB=postgres',
+        'INFRABOX_DATABASE_USER=postgres',
+        'INFRABOX_DATABASE_PORT=5432',
+        'INFRABOX_DATABASE_PASSWORD=postgres'
+    ],
+    'links': ['postgres', 'nginx-ingress'],
+    'networks': ['infrabox'],
+    'volumes': copy.deepcopy(data['services']['dashboard-api']['volumes'])
+}
+
+with open('/infrabox/output/e2e/compose/docker-compose.yml', 'w') as o:
+    yaml.dump(data, o, default_flow_style=False)
