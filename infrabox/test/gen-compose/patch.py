345e7d45b044f908fee7a152e6c05066b84b3d75
Steffen <steffen@infrabox.net>
2018-02-11 14:20:12 +0100

    add missing env var

--- infrabox/test/gen-compose/patch.py
+++ infrabox/test/gen-compose/patch.py
@@ -31,7 +31,8 @@ data['services']['test'] = {
         'INFRABOX_DATABASE_USER=postgres',
         'INFRABOX_DATABASE_PORT=5432',
         'INFRABOX_DATABASE_PASSWORD=postgres',
-        'INFRABOX_ROOT_URL=http://nginx-ingress'
+        'INFRABOX_ROOT_URL=http://nginx-ingress',
+        'INFRABOX_URL=http://nginx-ingress'
     ],
     'links': ['postgres', 'nginx-ingress'],
     'networks': ['infrabox'],
