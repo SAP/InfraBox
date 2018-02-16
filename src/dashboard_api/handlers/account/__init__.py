c7b06ba444d5222b0b6d0b3453c56ba2c197a077
Steffen Geissinger <steffen@infrabox.net>
2018-02-16 15:10:06 +0100

    fixes

--- src/dashboard_api/handlers/account/__init__.py
+++ src/dashboard_api/handlers/account/__init__.py
@@ -4,5 +4,6 @@ if os.environ['INFRABOX_ACCOUNT_SIGNUP_ENABLED'] == 'true':
     import dashboard_api.handlers.account.account
 elif os.environ['INFRABOX_ACCOUNT_LDAP_ENABLED'] == 'true':
     import dashboard_api.handlers.account.account_ldap
-elif os.environ['INFRABOX_GITHUB_ENABLED'] == 'true':
+
+if os.environ['INFRABOX_GITHUB_ENABLED'] == 'true':
     import dashboard_api.handlers.account.github
