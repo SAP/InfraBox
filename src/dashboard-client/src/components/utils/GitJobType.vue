082756cd6bd858122de11360aa07d811faa851e5
Steffen <steffen@infrabox.net>
2018-02-23 18:56:00 +0100

    fixes

--- src/dashboard-client/src/components/utils/GitJobType.vue
+++ src/dashboard-client/src/components/utils/GitJobType.vue
@@ -7,14 +7,14 @@
                 <span v-if="showTitle">Tag</span>
             </a>
         </div>
-        <div v-if="build.commit && !build.commit.tag && !build.pull_request">
+        <div v-if="build.commit && !build.commit.tag && (!build.pull_request || !build.pull_request.url)">
             <a target="_blank" :href="build.commit.url">
                 <img src="../../../static/git-commit.svg">
                 <span v-if="showTitle">Commit</span>
                 <md-tooltip>Commit</md-tooltip>
             </a>
         </div>
-        <div v-if="build.pull_request">
+        <div v-if="build.pull_request && build.pull_request.url">
             <a target="_blank" :href="build.pull_request.url">
                 <img src="../../../static/git-pull-request.svg">
                 <span v-if="showTitle">Pull request</span>
