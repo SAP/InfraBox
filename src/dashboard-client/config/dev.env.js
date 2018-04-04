62f97fb43ac630ff129bf43d4a5fa759da8292f8
Steffen <steffen@infrabox.net>
2018-04-04 12:45:00 +0200

    fix missing env var

--- src/dashboard-client/config/dev.env.js
+++ src/dashboard-client/config/dev.env.js
@@ -5,5 +5,5 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   DASHBOARD_HOST: '"localhost:8080"',
-  NEW_API_PATH: '"http://192.168.1.31:30080/api/v1/"'
+  NEW_API_PATH: '"http://localhost:8080/api/v1/"'
 })
