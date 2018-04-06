75e8b0ddc92c3cab1471c3d327fa3387e25d89bc
Steffen <steffen@infrabox.net>
2018-04-06 17:07:26 +0200

    remove additional ingresses

--- src/pyinfraboxutils/ibrestplus.py
+++ src/pyinfraboxutils/ibrestplus.py
@@ -1,18 +1,36 @@
 from flask_restplus import Api, abort as restplus_abort
+from flask_restplus.api import SwaggerView
 
 from pyinfraboxutils.ibflask import app
 
-authorizations = {
-    'TokenAuth': {
-        'type': 'basic'
-    }
-}
+class IBApi(Api):
+    def __init__(self):
+        authorizations = {
+            'TokenAuth': {
+                'type': 'basic'
+            }
+        }
 
-api = Api(app,
-          authorizations=authorizations,
-          security=['TokenAuth'],
-          doc='/api/doc/',
-          validate=True)
+        super(IBApi, self).__init__(app,
+                                    authorizations=authorizations,
+                                    security=['TokenAuth'],
+                                    doc='/api/doc/',
+                                    validate=True,
+                                    add_specs=False)
+
+    def _register_specs(self, app_or_blueprint):
+        endpoint = str('specs')
+        self._register_view(
+            app_or_blueprint,
+            SwaggerView,
+            '/api/swagger.json',
+            endpoint=endpoint,
+            resource_class_args=(self, )
+        )
+
+        self.endpoints.add(endpoint)
+
+api = IBApi()
 
 def abort(code, message, data=None):
     restplus_abort(code, message, data=data)
