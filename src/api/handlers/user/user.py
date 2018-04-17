0753874244693e1dfce2ecca0c9b74379bbb4cd1
Kirill Abramov <gammerxpower@gmail.com>
2018-04-17 16:53:50 +0200

    New CLI API: removed method to query user id by name.

--- src/api/handlers/user/user.py
+++ src/api/handlers/user/user.py
@@ -22,20 +22,3 @@ class User(Resource):
             abort(404)
 
         return user
-
-
-@ns.route('/id/<username>')
-class User(Resource):
-
-    @auth_required(['user'], check_project_access=False)
-    def get(self, username):
-        user_id = g.db.execute_one('''
-            SELECT id
-            FROM "user"
-            WHERE username = %s
-        ''', [username])
-
-        if not user_id:
-            abort(400, 'User not found.')
-
-        return OK('User exists.', { 'user_id': user_id[0] })
