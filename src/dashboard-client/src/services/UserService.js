89322557076bf7715fc85dfd01f1ec026f9bfbfe
Steffen <steffen@infrabox.net>
2018-04-23 09:35:45 +0200

    Fix logout did not remove the user from the store, fixes #314

--- src/dashboard-client/src/services/UserService.js
+++ src/dashboard-client/src/services/UserService.js
@@ -14,6 +14,11 @@ class UserService {
         this.init()
     }
 
+    logout () {
+        document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/; Max-Age=0'
+        store.commit('setUser', null)
+    }
+
     _loadSettings () {
         return NewAPIService.get(`settings/`)
             .then((s) => {
