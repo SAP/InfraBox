2a8815cc8d688a2c3a855185ebdada0e78756755
Steffen <steffen@infrabox.net>
2018-05-13 09:55:31 +0200

    remove debug printing

--- infrabox/test/e2e/entrypoint.sh
+++ infrabox/test/e2e/entrypoint.sh
@@ -57,16 +57,12 @@ _getPodNameImpl() {
 }
 
 _getNginxIP() {
-    kubectl get services --all-namespaces
-
     external_ip=""
     while [ -z $external_ip ]; do
-        echo "Waiting for nginx endpoint..."
         external_ip=$(kubectl -n kube-system get svc nic-nginx-ingress-controller --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}")
         [ -z "$external_ip" ] && sleep 10
     done
 
-    echo "Nginx endpoint: $external_ip"
     echo $external_ip
 }
 
