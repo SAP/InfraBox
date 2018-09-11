d917ce32f780294e0eb3bd8c809dd1234bd4b396
Steffen Geissinger <steffen@infrabox.net>
2018-09-11 15:44:36 +0200

    fixes

--- src/db/migrations/00016.sql
+++ src/db/migrations/00016.sql
@@ -1,1 +1,2 @@
-ALTER TABLE job DROP COLUMN memory, DROP COLUMN cpu, DROP COLUMN build_only, DROP COLUMN timeout, DROP COLUMN security_context, DROP COLUM resources;
+ALTER TABLE job DROP COLUMN memory, DROP COLUMN cpu, DROP COLUMN build_only, DROP COLUMN timeout, DROP COLUMN security_context, DROP COLUMN resources;
+ALTER TABLE job ALTER COLUMN definition SET NOT NULL;
