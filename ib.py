a2f842c41abed011f48ac5c1ebe652412dc53238
Damien Jochim <damien.jochim@sap.com>
2018-09-13 13:56:52 +0200

    Start opa and copy rego files

--- ib.py
+++ ib.py
@@ -150,9 +150,12 @@ def services_start(args):
         run = os.path.join(p, 'run_with_dummy.sh')
         execute(['bash', run], cwd=p)
     elif args.service_name == 'opa':
-        p = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'src', 'openpolicyagent')
-        run = os.path.join(['docker', 'run', '-ti', '--rm', '-p', '8181:8181', 'openpolicyagent/opa:0.9.1', 'run', '--server', '--log-level=debug'])
+        run = ['docker', 'run', '-ti', '--rm', '-p', '8181:8181', 'openpolicyagent/opa:0.9.1', 'run', '--server', '--log-level=debug']
         execute(run)
+    elif args.service_name == 'opacopy':
+        p = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'src', 'openpolicyagent')
+        run = os.path.join(p, 'run_opa.sh')
+        execute(['bash', run], cwd=p)
     elif args.service_name == 'dashboard-client':
         p = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'src', 'dashboard-client')
         execute(['npm', 'run', 'dev'], cwd=p)
---
+++ src/openpolicyagent/run_opa.sh
@@ -0,0 +1,9 @@
+# docker run --rm -p 8181:8181 openpolicyagent/opa:0.9.1 run --server --log-level=debug
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
+export PYTHONPATH=$PYTHONPATH:$SCRIPTPATH/..
+echo $PYTHONPATH
+
+export INFRABOX_OPA_HOST=http://wdfl33986622b.emea.global.corp.sap:8181
+
+python opa.py
