a39a7246e8aa3b0dec3ee1aea7c3d3b26f08b938
Joerg Bastian <joerg.bastian@sap.com>
2018-09-18 12:29:39 +0200

    Plugged in OPA cable

--- src/pyinfraboxutils/ibflask.py
+++ src/pyinfraboxutils/ibflask.py
@@ -1,6 +1,6 @@
 import base64
-import requests
 import json
+import requests
 
 from functools import wraps
 
@@ -14,6 +14,82 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 logger = get_logger('ibflask')
+
+try:
+    #pylint: disable=ungrouped-imports,wrong-import-position
+    from pyinfraboxutils import dbpool
+    logger.info('Using DB Pool')
+
+    @app.before_request
+    def before_request():
+        g.db = dbpool.get()
+
+        g.token = normalize_token(get_token())
+        check_request_authorization()
+
+        def release_db():
+            db = getattr(g, 'db', None)
+            if not db:
+                return
+
+            dbpool.put(db)
+            g.db = None
+
+        g.release_db = release_db
+
+except:
+    @app.before_request
+    def before_request():
+        g.db = DB(connect_db())
+
+        g.token = normalize_token(get_token())
+        check_request_authorization()
+
+        def release_db():
+            db = getattr(g, 'db', None)
+            if not db:
+                return
+
+            db.close()
+            g.db = None
+
+        g.release_db = release_db
+
+@app.teardown_request
+def teardown_request(_):
+    try:
+        release_db = getattr(g, 'release_db', None)
+        if release_db:
+            release_db()
+    except Exception as e:
+        logger.error(_)
+        logger.exception(e)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    msg = error.description
+
+    if not msg:
+        msg = 'Not Found'
+
+    return jsonify({'message': msg, 'status': 404}), 404
+
+@app.errorhandler(401)
+def unauthorized(error):
+    return jsonify({'message': error.description, 'status': 401}), 401
+
+@app.errorhandler(400)
+def bad_request(error):
+    return jsonify({'message': error.description, 'status': 400}), 400
+
+def OK(message, data=None):
+    d = {'message': message, 'status': 200}
+
+    if data:
+        d['data'] = data
+
+    return jsonify(d)
 
 def get_token():
     auth = dict(request.headers).get('Authorization', None)
@@ -74,56 +150,8 @@ def require_token():
         abort(401, 'Unauthorized')
     return token
 
-try:
-    #pylint: disable=ungrouped-imports,wrong-import-position
-    from pyinfraboxutils import dbpool
-    logger.info('Using DB Pool')
-
-    @app.before_request
-    def before_request():
-        g.db = dbpool.get()
-
-        check_request_authorization()
-
-        def release_db():
-            db = getattr(g, 'db', None)
-            if not db:
-                return
-
-            dbpool.put(db)
-            g.db = None
-
-        g.release_db = release_db
-
-except:
-    @app.before_request
-    def before_request():
-        g.db = DB(connect_db())
-
-        check_request_authorization()
-
-        def release_db():
-            db = getattr(g, 'db', None)
-            if not db:
-                return
-
-            db.close()
-            g.db = None
-
-        g.release_db = release_db
-
 def check_request_authorization():
     try:
-        g.token = get_token()
-
-        # Enrich job token
-        if g.token is not None and "type" in g.token and g.token["type"] == "job":
-            g.token = enrich_job_token(g.token)
-
-        # Legacy
-        elif g.token is not None and "type" in g.token and g.token["type"] == "project-token":
-                g.token.type = 'project'
-
         # Assemble Input Data for Open Policy Agent
         opa_input = {
             "input": {
@@ -135,7 +163,7 @@ def check_request_authorization():
         logger.info("OPA Request: " + json.dumps(opa_input))  
 
         # Send request to Open Policy Agent and evaluate response      
-        rsp = requests.post(get_env('INFRABOX_OPA_HOST') + "/v1/data/infrabox/", data=json.dumps(opa_input))
+        rsp = requests.post(get_env('INFRABOX_OPA_HOST') + "/v1/data/infrabox/allow", data=json.dumps(opa_input))
         rsp_dict = rsp.json()
         logger.info("OPA Response: " + rsp.content)
 
@@ -143,49 +171,9 @@ def check_request_authorization():
             logger.info("Unauthorized request.")
             abort(401, 'Unauthorized')
 
-    except LookupError as e:
-        logger.info(e)
-        abort(401, 'Unauthorized')
     except requests.exceptions.RequestException as e:
         logger.info(e)
         abort(500, 'Authorization failed')
-
-
-@app.teardown_request
-def teardown_request(_):
-    try:
-        release_db = getattr(g, 'release_db', None)
-        if release_db:
-            release_db()
-    except Exception as e:
-        logger.error(_)
-        logger.exception(e)
-
-
-@app.errorhandler(404)
-def not_found(error):
-    msg = error.description
-
-    if not msg:
-        msg = 'Not Found'
-
-    return jsonify({'message': msg, 'status': 404}), 404
-
-@app.errorhandler(401)
-def unauthorized(error):
-    return jsonify({'message': error.description, 'status': 401}), 401
-
-@app.errorhandler(400)
-def bad_request(error):
-    return jsonify({'message': error.description, 'status': 400}), 400
-
-def OK(message, data=None):
-    d = {'message': message, 'status': 200}
-
-    if data:
-        d['data'] = data
-
-    return jsonify(d)
 
 def token_required(f):
     @wraps(f)
@@ -217,26 +205,19 @@ def check_job_belongs_to_project(f):
         return f(*args, **kwargs)
     return decorated_function
 
-    job_id = token['job']['id']
-    r = g.db.execute_one('''
-        SELECT state, project_id, name
-        FROM job
-        WHERE id = %s''', [job_id])
+def normalize_token(token):
+    # Enrich job token
+    if token is not None and "type" in token and token["type"] == "job":
+        try:
+            return enrich_job_token(token)
+        except LookupError as e:
+            logger.info(e)
+            abort(401, 'Unauthorized')
+    # Legacy
+    if token is not None and "type" in token and token["type"] == "project-token":
+        g.token.type = 'project'
 
-    if not r:
-        logger.warn('job not found')
-        abort(401, 'Unauthorized')
-
-    job_state = r[0]
-    if job_state not in ('queued', 'running', 'scheduled'):
-        abort(401, 'Unauthorized')
-
-
-    token['job']['state'] = r[0]
-    token['job']['name'] = r[2]
-    token['project'] = {}
-    token['project']['id'] = r[1]
-    g.token = token
+    return token
 
 def enrich_job_token(token):
     job_id = token['job']['id']
