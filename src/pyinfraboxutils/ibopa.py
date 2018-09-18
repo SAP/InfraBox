7379d8a58c291543306fa4e74b8f6c0d66cc52bc
Joerg Bastian <joerg.bastian@sap.com>
2018-09-18 16:55:31 +0200

    Moved auth function from ibflask.py to ibopa.py

--- src/pyinfraboxutils/ibopa.py
+++ src/pyinfraboxutils/ibopa.py
@@ -3,15 +3,35 @@ import requests
 
 from pyinfraboxutils import get_logger, get_env, dbpool
 
-
 logger = get_logger('OPA')
 
-def opa_push_data():
-    db = dbpool.get()
-    opa_push_collaborator_data(db)
-    opa_push_project_data(db)
+OPA_AUTH_URL = "%s/v1/data/infrabox/allow" % get_env('INFRABOX_OPA_HOST')
+COLLABORATOR_DATA_DEST_URL = "%s/v1/data/infrabox/collaborators" % get_env("INFRABOX_OPA_HOST")
+PROJECT_DATA_DEST_URL = "%s/v1/data/infrabox/projects" % get_env("INFRABOX_OPA_HOST")
 
+def opa_do_auth(input_dict):
+    # Send request to Open Policy Agent and evaluate response
+    payload = json.dumps(input_dict)
+    logger.debug("Sending OPA Request: %s", payload)
+    try:
+        rsp = requests.post(OPA_AUTH_URL, data=json.dumps(input_dict))
+        rsp_dict = rsp.json()
+        logger.debug("OPA Response: %s", rsp.content)
 
+        return "result" in rsp_dict and rsp_dict["result"] is True
+
+    except requests.exceptions.RequestException as e:
+        raise e
+
+def opa_push_data(destination_url, json_payload):
+    try:
+        rsp = requests.put(destination_url, data=json_payload, headers={"Content-Type" : "application/json"})
+        if rsp:
+            logger.debug("Pushed data to %s (Status %s):%s", destination_url, str(rsp.status_code), json_payload)
+        else:
+            logger.error("Failed pushing data to %s (Status %s): Req.: %s; Resp.: %s", destination_url, str(rsp.status_code), json_payload, rsp.content)
+    except requests.exceptions.RequestException as e:
+        logger.exception("Failed pushing data to %s: %s; Req.: %s", destination_url, e, json_payload) 
 
 def opa_push_collaborator_data(db):
     collaborators = db.execute_many_dict(
@@ -19,16 +39,7 @@ def opa_push_collaborator_data(db):
         SELECT user_id, project_id, role FROM collaborator
     """)
     payload = json.dumps({'collaborators': collaborators})
-    try:
-        rsp = requests.put(get_collaborator_data_destination(), data=payload, headers={"Content-Type" : "application/json"})
-        if rsp:
-            logger.debug("Pushed collaborator data to Open Policy Agent (Status %s):%s", str(rsp.status_code), payload)
-        else:
-            logger.error("Failed pushing collaborator data to Open Policy Agent (Status %s): Req.: %s; Resp.: %s", str(rsp.status_code), payload, rsp.content)
-    except requests.exceptions.RequestException as e:
-        logger.exception("Failed pushing collaborator data to Open Policy Agent: %s; Req.: %s", e, payload)
-
-
+    opa_push_data(COLLABORATOR_DATA_DEST_URL, payload)
 
 def opa_push_project_data(db):
     projects = db.execute_many_dict(
@@ -37,18 +48,9 @@ def opa_push_project_data(db):
     """
     )
     payload = json.dumps({"projects": projects})
+    opa_push_data(PROJECT_DATA_DEST_URL, payload)
 
-    try:
-        rsp = requests.put(get_project_data_destination(), data=payload, headers={"Content-Type" : "application/json"})
-        if rsp:
-            logger.debug("Pushed project data to Open Policy Agent (Status %s):%s", str(rsp.status_code), payload)
-        else:
-            logger.error("Failed pushing project data to Open Policy Agent (Status %s): Req.: %s; Resp.: %s", str(rsp.status_code), payload, rsp.content)
-    except requests.exceptions.RequestException as e:
-        logger.exception("Failed pushing project data to Open Policy Agent: %s; Req.: %s", e, payload)
-
-def get_collaborator_data_destination():
-    return "%s/v1/data/infrabox/collaborators" % get_env("INFRABOX_OPA_HOST")
-
-def get_project_data_destination():
-    return "%s/v1/data/infrabox/projects" % get_env("INFRABOX_OPA_HOST")
+def opa_push_all():
+    db = dbpool.get()
+    opa_push_collaborator_data(db)
+    opa_push_project_data(db)
