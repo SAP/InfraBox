9fc85ddff8c9d135cf122996c6936b797d1a2dc6
Joerg Bastian <joerg.bastian@sap.com>
2018-09-19 13:27:07 +0200

    OPA for docker-registry-auth

--- src/docker-registry/auth/server.py
+++ src/docker-registry/auth/server.py
@@ -13,35 +13,36 @@ def ping():
 @app.route('/v2')
 @token_required
 def v2():
-    token = g.token
-    if token['type'] == 'project':
-        r = g.db.execute_many('''
-            SELECT id FROM auth_token
-            WHERE id = %s AND project_id = %s
-        ''', (token['id'], token['project']['id'],))
+    # Replaced - see authorization in src/opa/policies/docker-registry-auth.rego
+    # token = g.token
+    # if token['type'] == 'project':
+    #     r = g.db.execute_many('''
+    #         SELECT id FROM auth_token
+    #         WHERE id = %s AND project_id = %s
+    #     ''', (token['id'], token['project']['id'],))
 
-        if len(r) != 1:
-            logger.warn('project not found')
-            abort(401, 'Unauthorized')
+    #     if len(r) != 1:
+    #         logger.warn('project not found')
+    #         abort(401, 'Unauthorized')
 
-    elif token['type'] == 'job':
-        r = g.db.execute_many('''
-            SELECT state FROM job
-            WHERE id = %s
-        ''', (token['job']['id'],))
+    # elif token['type'] == 'job':
+    #     r = g.db.execute_many('''
+    #         SELECT state FROM job
+    #         WHERE id = %s
+    #     ''', (token['job']['id'],))
 
-        if len(r) != 1:
-            logger.warn('job not found')
-            abort(401, 'Unauthorized')
+    #     if len(r) != 1:
+    #         logger.warn('job not found')
+    #         abort(401, 'Unauthorized')
 
-        state = r[0][0]
+    #     state = r[0][0]
 
-        if state not in ('scheduled', 'running'):
-            logger.warn('job not running anymore: %s' % token['job']['id'])
-            abort(401, 'Unauthorized')
-    else: # pragma: no cover
-        logger.warn('unsupported token type: %s' % token['type'])
-        abort(401, 'Unauthorized')
+    #     if state not in ('scheduled', 'running'):
+    #         logger.warn('job not running anymore: %s' % token['job']['id'])
+    #         abort(401, 'Unauthorized')
+    # else: # pragma: no cover
+    #     logger.warn('unsupported token type: %s' % token['type'])
+    #     abort(401, 'Unauthorized')
 
     return jsonify({'status': 200})
 
@@ -49,61 +50,61 @@ def v2():
 @app.route('/v2/<path:path>')
 @token_required
 def v2_path(path):
-    p = path.split('/')
+    # p = path.split('/')
     method = dict(request.headers).get('X-Original-Method', None)
 
     if not method:
         logger.warn('no x-original-method header')
         abort(401, 'Unauthorized')
 
-    if len(p) < 2:
-        logger.warn('invalid repo')
-        abort(401, 'Unauthorized')
+    # if len(p) < 2:
+    #     logger.warn('invalid repo')
+    #     abort(401, 'Unauthorized')
 
-    project_id = p[0]
-    token = g.token
+    # project_id = p[0]
+    # token = g.token
 
-    if token['type'] == 'project':
-        if method != 'GET':
-            logger.warn('%s not allowed with project token', request.method)
-            abort(401, 'Unauthorized')
+    # if token['type'] == 'project':
+    #     if method != 'GET':
+    #         logger.warn('%s not allowed with project token', request.method)
+    #         abort(401, 'Unauthorized')
 
-        if project_id != token['project']['id']:
-            logger.warn('token is not valid for project')
-            abort(401, 'Unauthorized')
+    #     if project_id != token['project']['id']:
+    #         logger.warn('token is not valid for project')
+    #         abort(401, 'Unauthorized')
 
-        r = g.db.execute_many('''
-            SELECT * FROM auth_token
-            WHERE id = %s AND project_id = %s
-        ''', (token['id'], token['project']['id']))
+    #     r = g.db.execute_many('''
+    #         SELECT * FROM auth_token
+    #         WHERE id = %s AND project_id = %s
+    #     ''', (token['id'], token['project']['id']))
 
-        if len(r) != 1:
-            logger.warn('project not found')
-            abort(401, 'Unauthorized')
-    elif token['type'] == 'job':
-        r = g.db.execute_one('''
-            SELECT state, project_id FROM job
-            WHERE id = %s
-        ''', (token['job']['id'],))
+    #     if len(r) != 1:
+    #         logger.warn('project not found')
+    #         abort(401, 'Unauthorized')
+    # elif token['type'] == 'job':
+    #     r = g.db.execute_one('''
+    #         SELECT state, project_id FROM job
+    #         WHERE id = %s
+    #     ''', (token['job']['id'],))
 
-        if not r:
-            logger.warn('job not found')
-            abort(401, 'Unauthorized')
+    #     if not r:
+    #         logger.warn('job not found')
+    #         abort(401, 'Unauthorized')
 
-        state = r[0]
-        job_project_id = r[1]
+    #     state = r[0]
+    #     job_project_id = r[1]
 
-        if state not in ('scheduled', 'running'):
-            logger.warn('job not running anymore: %s' % token['job']['id'])
-            abort(401, 'Unauthorized')
+    #     if state not in ('scheduled', 'running'):
+    #         logger.warn('job not running anymore: %s' % token['job']['id'])
+    #         abort(401, 'Unauthorized')
 
-        if project_id != job_project_id:
-            logger.warn('job does not belong to project')
-            abort(401, 'Unauthorized')
+    #     if project_id != job_project_id:
+    #         logger.warn('job does not belong to project')
+    #         abort(401, 'Unauthorized')
 
-    else: # pragma: no cover
-        logger.warn('unsupported token type')
-        abort(401, 'Unauthorized')
+    # else: # pragma: no cover
+    #     logger.warn('unsupported token type')
+    #     abort(401, 'Unauthorized')
 
     return jsonify({'status': 200})
 
--- src/openpolicyagent/policies/docker-registry-auth.rego
+++ src/openpolicyagent/policies/docker-registry-auth.rego
@@ -2,9 +2,47 @@ package infrabox
 
 import input as api
 
+import data.infrabox.projects.projects
+
 # Allow GET access to /ping for all
 allow {
     api.method = "GET"
     api.path = ["ping"]
 }
 
+# Allow access to /v2
+allow {
+    api.path = ["v2"]
+    api.token.type = "project"
+}
+
+allow {
+    api.path = ["v2"]
+    api.token.type = "job"
+    states := ["scheduled", "running"]
+    api.token.job.state = states[_]
+}
+
+# Allow access to /v2/path
+allow {
+    api.method = "GET"
+    api.path = ["v2", project_id]
+    count(project_id, project_id_length)
+    project_id_length >= 2
+
+    api.token.type = "project"
+    api.token.project.id = project_id
+    project_id = projects[_].id
+}
+
+allow {
+    api.path = ["v2", project_id]
+    count(project_id, project_id_length)
+    project_id_length >= 2
+
+    api.token.type = "job"
+    states := ["scheduled", "running"]
+    api.token.job.state = states[_]
+    api.token.project.id = project_id
+    project_id = projects[_].id
+}
