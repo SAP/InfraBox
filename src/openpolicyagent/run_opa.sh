c7781fc3b9ed1c2a9fa9bfb210cb9f1b7df53e95
Damien Jochim <damien.jochim@sap.com>
2018-09-21 15:39:25 +0200

    Add Opa to docker-compose test and split opa host and port

--- src/openpolicyagent/run_opa.sh
+++ src/openpolicyagent/run_opa.sh
@@ -4,6 +4,7 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 export PYTHONPATH=$PYTHONPATH:$SCRIPTPATH/..
 echo $PYTHONPATH
 
-export INFRABOX_OPA_HOST=http://localhost:8181
+export INFRABOX_OPA_HOST=wdfl33986622b.emea.global.corp.sap
+export INFRABOX_OPA_PORT=8181
 
 python opa.py
