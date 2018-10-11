9e662e3009e67b0e9a5c928a8eea9d24adff51a4
Aurelien Caille <aurelien.caille@sap.com>
2018-10-11 11:28:44 +0200

    PULL

--- src/dashboard-client/src/components/job/JobDetail.vue
+++ src/dashboard-client/src/components/job/JobDetail.vue
@@ -231,10 +231,11 @@ export default {
                         return j.loadTests()
                     })
                     .then(() => {
+                        console.log(this.job.tests)
                         this.failedTests = 0
                         for (let t of this.job.tests) {
                             console.log(t.state)
-                            if (t.state === 'failure' || t.state === 'error') {
+                            if (t.state === 'failed' || t.state === 'error') {
                                 this.failedTests += 1
                             }
                         }
