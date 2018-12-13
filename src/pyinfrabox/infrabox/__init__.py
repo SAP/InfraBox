4a3e96ee0498c87c56d214ea0bbfc3614269d41f
Rui Ban <iberryful@gmail.com>
2018-12-13 13:22:41 +0800

    add tests for circular dependency.

--- src/pyinfrabox/infrabox/__init__.py
+++ src/pyinfrabox/infrabox/__init__.py
@@ -550,12 +550,12 @@ def validate_json(d):
         if deps:
             all_deps[job_name] = deps
 
-    for job_name, deps in all_deps:
-        queue = list(deps.iterkeys())
+    for job_name, deps in all_deps.items():
+        queue = list(deps.keys())
         for dep_job in queue:
             if dep_job == job_name:
-                raise ValidationError("Circular dependency detected. Job %s <-> Job %s" % (job_name, dep_job))
+                raise ValidationError("Circular dependency detected.")
             if dep_job in all_deps:
-                queue.extend(all_deps[dep_job].iterkeys())
+                queue.extend(all_deps[dep_job].keys())
 
     return True
