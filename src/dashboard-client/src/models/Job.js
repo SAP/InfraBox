3d9c80418e059f7d78b0a8f2921e09822b7b3f8d
Steffen Geissinger <steffen@infrabox.net>
2019-02-14 03:40:53 -0800

    Fix #275

--- src/dashboard-client/src/models/Job.js
+++ src/dashboard-client/src/models/Job.js
@@ -72,11 +72,11 @@ class Section {
 
     escapeHtml (text) {
         const map = {
-            '&': '&amp',
-            '<': '&lt',
-            '>': '&gt',
-            '"': '&quot',
-            "'": '&#039'
+            '&': '&amp;',
+            '<': '&lt;',
+            '>': '&gt;',
+            '"': '&quot;',
+            "'": '&#039;'
         }
 
         return text.replace(/[&<>'']/g, (m) => map[m])
