f17bba78c686851f6751a9f9b9a052d624f9f384
berry <iberryful@gmail.com>
2020-11-26 10:50:10 +0800

    fix(integration-test): fix rsa public key path

--- infrabox/test/e2e/entrypoint.sh
+++ infrabox/test/e2e/entrypoint.sh
@@ -139,11 +139,14 @@ _installNginxIngress() {
 _installInfrabox() {
 	cd /infrabox/context/deploy/infrabox
 
-    ssh-keygen -N '' -t rsa -f id_rsa
+    ssh-keygen -N '' -t rsa -m pem -f id_rsa
     ssh-keygen -f id_rsa.pub -e -m pem > id_rsa.pem
 
     mkdir -p /var/run/secrets/infrabox.net/rsa/
     cp id_rsa* /var/run/secrets/infrabox.net/rsa/
+
+    export INFRABOX_RSA_PRIVATE_KEY_PATH=/var/run/secrets/infrabox.net/rsa/id_rsa
+    export INFRABOX_RSA_PUBLIC_KEY_PATH=/var/run/secrets/infrabox.net/rsa/id_rsa.pem
 
     echo "## Install infrabox"
 
