a0c1f9dee8b1fccbb569bad12f611cdddd22a75d
I517992 <frank.cheng02@sap.com>
2021-02-01 17:58:25 +0800

    add separator

--- src/db/migrations/00036.sql
+++ src/db/migrations/00036.sql
@@ -3,6 +3,6 @@ CREATE TABLE vault (
     project_id uuid NOT NULL,
     url VARCHAR NOT NULL,
     token VARCHAR NOT NULL
-)
+);
 
 CREATE INDEX vault_project_id_idx ON vault USING btree (project_id);
