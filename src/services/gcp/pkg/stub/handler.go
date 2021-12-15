5d8d520acf066c1b5a55993810e3d1902a1d2fda
I517992 <frank.cheng02@sap.com>
2021-12-15 16:41:46 +0800

    bug fix

--- src/services/gcp/pkg/stub/handler.go
+++ src/services/gcp/pkg/stub/handler.go
@@ -147,7 +147,7 @@ func createCluster(cr *v1alpha1.GKECluster, log *logrus.Entry) (*v1alpha1.GKEClu
         args = append(args, "--enable-legacy-authorization")
     }
 
-    if !cr.Spec.EnablePodSecurityPolicy {
+    if cr.Spec.EnablePodSecurityPolicy {
         args = append(args, "--enable-pod-security-policy")
     }
     if cr.Spec.NumNodes != 0 {
