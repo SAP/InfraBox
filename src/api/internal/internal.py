b37695cd95bb17307eded5ef2ef9f19bed15e924
I517992 <frank.cheng02@sap.com>
2022-02-22 18:32:33 +0800

    remove debug code

--- src/api/internal/internal.py
+++ src/api/internal/internal.py
@@ -33,7 +33,6 @@ class ConsoleUpdate(Resource):
             if 'kubernetes' not in r:
                 continue
 
-            print(r)
             job_id = r['kubernetes']['labels']['job-name'][:-4]
 
             a = float(r['date'])
