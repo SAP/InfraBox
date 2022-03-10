9da1b219273c9e418a8ee78e72179ca66d42221c
VAN-QIAN <1343544792@qq.com>
2022-03-10 14:50:25 +0800

    fix

--- src/job/job.py
+++ src/job/job.py
@@ -743,7 +743,9 @@ class RunJob(Job):
                 cmds.append('--parallel')
 
             # check if enable buildkit
-            os.environ['DOCKER_BUILDKIT'] = '1'
+            if  self.job['definition'].get('enable_docker_build_kit', False) is True:
+                os.environ['DOCKER_BUILDKIT'] = '1'
+                os.environ['COMPOSE_DOCKER_CLI_BUILD']= '1'
 
             c.execute_mask(cmds,
                       show=True, env=self.environment, mask=self.repository.get('github_api_token', None))
