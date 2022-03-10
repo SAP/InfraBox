51bbeeba5c60701731a5a5f69e0e1ba1a2717f56
VAN-QIAN <1343544792@qq.com>
2022-03-10 21:34:21 +0800

    remove CLI

--- src/job/job.py
+++ src/job/job.py
@@ -749,8 +749,9 @@ class RunJob(Job):
 
             if  self.job['definition'].get('enable_docker_build_kit', False) is True:
                 os.environ['DOCKER_BUILDKIT'] = '1'
-                os.environ['COMPOSE_DOCKER_CLI_BUILD']= '1'
+                # os.environ['COMPOSE_DOCKER_CLI_BUILD']= '1'
                 c.collect("BUILDKIT is enable! DOCKER_BUILDKIT is {docker},COMPOSE_DOCKER_CLI_BUILD is {compose}".format(os.environ['DOCKER_BUILDKIT'],os.environ['COMPOSE_DOCKER_CLI_BUILD']), show=True)
+
             c.execute_mask(cmds,
                       show=True, env=self.environment, mask=self.repository.get('github_api_token', None))
             c.header("Run docker-compose", show=True)
