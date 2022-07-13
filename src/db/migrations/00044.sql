8efd8923b7ba728d853c42c6b6f6bde2cc2e92b0
I517992 <frank.cheng02@sap.com>
2022-07-13 17:14:06 +0800

    chore: add quote

--- src/db/migrations/00044.sql
+++ src/db/migrations/00044.sql
@@ -1,5 +1,5 @@
 ALTER TABLE "project" DROP COLUMN build_skip_pattern;
-CREATE TABLE project_skip_pattern (
+CREATE TABLE "project_skip_pattern" (
     id uuid DEFAULT gen_random_uuid() NOT NULL,
     project_id uuid NOT NULL,
     skip_pattern TEXT,
