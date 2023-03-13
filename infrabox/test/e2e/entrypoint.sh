43f810dbed458dea865a69c4426eb9aa308f0baf
Jiacheng Cheng <jiacheng.cheng@sap.com>
2023-03-13 10:37:52 +0800

    fix _prepareKubectl

--- infrabox/test/e2e/entrypoint.sh
+++ infrabox/test/e2e/entrypoint.sh
@@ -7,29 +7,26 @@ _prepareKubectl() {
 
 	SERVICE_NAME="e2e-cluster"
 
-	CA_CRT="/var/run/infrabox.net/services/$SERVICE_NAME/ca.crt"
-	CLIENT_CRT="/var/run/infrabox.net/services/$SERVICE_NAME/client.crt"
-	CLIENT_KEY="/var/run/infrabox.net/services/$SERVICE_NAME/client.key"
+    CA_CRT="/var/run/infrabox.net/services/$SERVICE_NAME/ca.crt"
 
+    ENDPOINT=$(cat /var/run/infrabox.net/services/$SERVICE_NAME/endpoint)
+    TOKEN=$(cat /var/run/infrabox.net/services/$SERVICE_NAME/token)
 
-	ENDPOINT=$(cat /var/run/infrabox.net/services/$SERVICE_NAME/endpoint)
-	PASSWORD=$(cat /var/run/infrabox.net/services/$SERVICE_NAME/password)
-	USERNAME=$(cat /var/run/infrabox.net/services/$SERVICE_NAME/username)
+    kubectl config set-cluster $SERVICE_NAME \
+        --server=$ENDPOINT \
+        --embed-certs=true \
+        --certificate-authority=$CA_CRT
 
-	kubectl config set-cluster $SERVICE_NAME \
-		--server=$ENDPOINT \
-		--certificate-authority=$CA_CRT
+    kubectl config set-credentials admin \
+        --token=$TOKEN
 
-	kubectl config set-credentials admin \
-		--certificate-authority=$CA_CRT \
-		--client-certificate=$CLIENT_CRT \
-		--client-key=$CLIENT_KEY \
-		--username=$USERNAME \
-		--password=$PASSWORD
+    kubectl config set-context default-system \
+        --cluster=$SERVICE_NAME \
+        --user=admin
 
-	kubectl config set-context default-system \
-		--cluster=$SERVICE_NAME \
-		--user=admin
+    kubectl config use-context default-system
+
+    kubectl get pods
 
     kubectl config use-context default-system
 
