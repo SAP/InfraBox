70c5aef16fbb40d3868f6119d3c7900437079a68
Jiacheng Cheng <jiacheng.cheng@sap.com>
2023-03-13 13:49:52 +0800

    fix name

--- infrabox/test/e2e/entrypoint.sh
+++ infrabox/test/e2e/entrypoint.sh
@@ -185,7 +185,7 @@ job:
         {"insecure-registries": ["$ROOT_URL"]}
 EOL
 
-    helm install --namespace infrabox-system -f my_values.yaml --wait .
+    helm install --namespace infrabox-system -f my_values.yaml --wait infrabox .
 
     export INFRABOX_DATABASE_HOST=localhost
     export INFRABOX_DATABASE_DB=postgres
