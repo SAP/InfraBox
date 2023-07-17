40a3758cdcb0e868ae9d8fd642f8b356be93668f
chengshifan <545591082@qq.com>
2023-07-17 10:04:46 +0800

    use 7 instead of 14 days

--- src/gc/gc.py
+++ src/gc/gc.py
@@ -145,7 +145,7 @@ class GC(object):
                 # FIXME: when migrated to Python3, we can just use fromisoformat
                 # last_modified = datetime.fromisoformat(obj["last_modified"])
                 last_modified = datetime.strptime(obj["last_modified"], "%Y-%m-%dT%H:%M:%S.%f")
-                if now - last_modified > timedelta(days=14):
+                if now - last_modified > timedelta(days=7):
                     storage._delete(obj["name"])
                     logger.info("deleted obj {}".format(obj['name']))
 
