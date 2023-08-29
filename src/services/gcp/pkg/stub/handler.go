6d2f2bfcd899d4fdd77dee37d3170489ba667fb2
chengshifan <545591082@qq.com>
2023-08-29 14:57:04 +0800

    bug fix

--- src/services/gcp/pkg/stub/handler.go
+++ src/services/gcp/pkg/stub/handler.go
@@ -872,8 +872,9 @@ func getOutdatedClusters(maxAge string, log *logrus.Entry) ([]RemoteCluster, err
 func getExistingMasterIPv4CIRDs(gkeclusters []RemoteCluster) []string {
 	var allMasterIPv4CIRSs = make([]string, len(gkeclusters))
 	for _, gkecluster := range gkeclusters {
-		if gkecluster.PrivateClusterConfig["masterIpv4CidrBlock"] != "" {
-			allMasterIPv4CIRSs = append(allMasterIPv4CIRSs, gkecluster.PrivateClusterConfig["masterIpv4CidrBlock"].(string))
+        masterIpv4CidrBlock, ok := gkecluster.PrivateClusterConfig["masterIpv4CidrBlock"].(string)
+		if ok && masterIpv4CidrBlock != "" {
+			allMasterIPv4CIRSs = append(allMasterIPv4CIRSs, masterIpv4CidrBlock)
 		}
 	}
 	return allMasterIPv4CIRSs
