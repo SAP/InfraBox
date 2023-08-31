b94a5af310394c3f3f58e211883cd8890f9bc5fe
chengshifan <545591082@qq.com>
2023-08-31 09:48:32 +0800

    bug fix

--- src/services/gcp/pkg/stub/handler.go
+++ src/services/gcp/pkg/stub/handler.go
@@ -222,7 +222,8 @@ func createCluster(cr *v1alpha1.GKECluster, log *logrus.Entry) (*v1alpha1.GKEClu
 	args = append(args, "--no-enable-legacy-authorization")
     args = append(args, "--enable-private-nodes")
 	args = append(args, "--master-ipv4-cidr", finalCIDR)
-	args = append(args, "--enable-master-authorized-networks", "0.0.0.0/0")
+	args = append(args, "--enable-master-authorized-networks")
+    args = append(args, "--master-authorized-networks", "0.0.0.0/0")
     cmd := exec.Command("gcloud" , args...)
     out, err := cmd.CombinedOutput()
 
